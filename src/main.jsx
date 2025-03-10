6b8e41873973575cc11d478b69c3a61c997c11ba
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2025-03-10 14:08:00 +0500

    male

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import ContextApiProvider from "./context/contextApi.js";
+import ContextApiProvider from "./context/contextApi.jsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
