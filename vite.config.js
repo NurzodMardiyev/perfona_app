c525c6d45e54bb992881fab7727cb4d781268757
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2025-04-23 13:44:14 +0500

    packeage

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // asosiy joylash uchun
+  base: "/", // asosiy joylash uchun
   server: {
     port: 5173, // standart Vite porti yoki o'zingiz xohlagan port
     strictPort: true,
   },
   build: {
-    outDir: './dist',
-  }
+    outDir: "./dist",
+  },
 });
